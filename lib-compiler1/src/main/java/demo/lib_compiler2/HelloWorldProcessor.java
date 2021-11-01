501e365fbe009877e24b210f7f1763c1fda6a7a6
guanliang.li <guanliang.li@ninebot.com>
2021-11-01 10:34:31 +0800

    :tada: createResource demo

--- lib-compiler1/src/main/java/demo/lib_compiler2/HelloWorldProcessor.java
+++ lib-compiler1/src/main/java/demo/lib_compiler2/HelloWorldProcessor.java
@@ -14,7 +14,9 @@ import javax.annotation.processing.SupportedSourceVersion;
 import javax.lang.model.SourceVersion;
 import javax.lang.model.element.Element;
 import javax.lang.model.element.TypeElement;
+import javax.tools.FileObject;
 import javax.tools.JavaFileObject;
+import javax.tools.StandardLocation;
 
 import demo.lib_annotation.HelloWorldGen;
 
@@ -49,6 +51,19 @@ public class HelloWorldProcessor extends AbstractProcessor {
                 writer.close();
             } catch (IOException e) {
                 System.out.println("HelloWorldProcessor -> gen file error, " + e);
+            }
+
+            try {
+                String resourceFile = "META-INF/services/test";
+                FileObject fileObject = processingEnv.getFiler().createResource(StandardLocation.CLASS_OUTPUT, "", resourceFile);
+                Writer writer = fileObject.openWriter();
+
+                writer.write("test string");
+
+                writer.flush();
+                writer.close();
+            } catch (IOException e) {
+                System.out.println("HelloWorldProcessor -> create resource error, " + e);
             }
         }
         return true;

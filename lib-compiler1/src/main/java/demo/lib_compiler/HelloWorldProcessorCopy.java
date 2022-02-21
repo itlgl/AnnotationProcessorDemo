6f807041ddc0cfde16c0d683f4cae8c9bd788d5b
itlgl <itlgl@outlook.com>
2022-02-21 17:33:48 +0800

    :tada 多个processor处理一个注解

--- lib-compiler1/src/main/java/demo/lib_compiler/HelloWorldProcessorCopy.java
+++ lib-compiler1/src/main/java/demo/lib_compiler/HelloWorldProcessorCopy.java
@@ -27,7 +27,7 @@ import demo.lib_annotation.HelloWorldGen;
 @AutoService(Processor.class)
 @SupportedSourceVersion(SourceVersion.RELEASE_8)
 //@SupportedAnnotationTypes("demo.lib_annotation.HelloWorldGen")
-public class HelloWorldProcessor extends AbstractProcessor {
+public class HelloWorldProcessorCopy extends AbstractProcessor {
 
     // 可用@SupportedAnnotationTypes("demo.lib_annotation.HelloWorldGen")替代
     @Override
@@ -38,20 +38,20 @@ public class HelloWorldProcessor extends AbstractProcessor {
     @Override
     public boolean process(Set<? extends TypeElement> annotations, RoundEnvironment roundEnv) {
         Set<? extends Element> elements = roundEnv.getElementsAnnotatedWith(HelloWorldGen.class);
-        System.out.println("HelloWorldProcessor -> this=" + this);
-        System.out.println("HelloWorldProcessor -> process elements.size()=" + elements.size());
-        System.out.println("HelloWorldProcessor -> roundEnv.processingOver()=" + roundEnv.processingOver());
+        System.out.println("HelloWorldProcessorCopy -> this=" + this);
+        System.out.println("HelloWorldProcessorCopy -> process elements.size()=" + elements.size());
+        System.out.println("HelloWorldProcessorCopy -> roundEnv.processingOver()=" + roundEnv.processingOver());
         if(elements.size() > 0) {
             // HelloWorld.java
             try {
                 JavaFileObject helloWorld = processingEnv.getFiler()
-                        .createSourceFile("demo.HelloWorld");
+                        .createSourceFile("demo.HelloWorldCopy");
 
                 Writer writer = helloWorld.openWriter();
 
                 writer.write("package demo;\n" +
                         "\n" +
-                        "public final class HelloWorld {\n" +
+                        "public final class HelloWorldCopy {\n" +
                         "  public static void main(String[] args) {\n" +
                         "    System.out.println(\"Hello jsr269!\");\n" +
                         "  }\n" +
@@ -59,37 +59,7 @@ public class HelloWorldProcessor extends AbstractProcessor {
                 writer.flush();
                 writer.close();
             } catch (IOException e) {
-                System.out.println("HelloWorldProcessor -> gen file error, " + e);
-            }
-
-            // META-INF/services/test
-            try {
-                String resourceFile = "META-INF/services/test";
-                FileObject fileObject = processingEnv.getFiler().createResource(StandardLocation.CLASS_OUTPUT, "", resourceFile);
-                Writer writer = fileObject.openWriter();
-
-                writer.write("test string");
-
-                writer.flush();
-                writer.close();
-            } catch (IOException e) {
-                System.out.println("HelloWorldProcessor -> create resource error, " + e);
-            }
-
-            File currDir = new File("");
-            System.out.println("curr dir=" + currDir.getAbsolutePath());
-
-            // read AndroidManifest.xml
-            File manifestFile = new File("./app/src/main/AndroidManifest.xml");
-            System.out.println("AndroidManifest absolute path exist=" + manifestFile.exists());
-
-            try {
-                BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(manifestFile), "utf-8"));
-                String s = br.readLine();
-                System.out.println(s);
-                br.close();
-            } catch (Exception e) {
-                e.printStackTrace(System.out);
+                System.out.println("HelloWorldProcessorCopy -> gen file error, " + e);
             }
         }
         return false;
